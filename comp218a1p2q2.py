d8beb498ce28c701ce7ad3cd03de34e4be8aad5b
Meshaal Khan <62823214+verycooljk007@users.noreply.github.com>
2025-03-20 22:51:24 -0400

    Add files via upload

---
+++ comp218a1p2q2.py
@@ -0,0 +1,14 @@
+# Program to print the figure using a for loop, my bad for any picture discrepancies
+
+
+lines = [
+    "     --------",
+    "    /       /",
+    "   /       /",
+    "  /       /",
+    " --------"
+]
+
+# Using a for loop to print each line
+for line in lines:
+    print(line)
