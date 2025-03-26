36dd2ddb401e9159ae188934b60f7af73fdd9723
Meshaal Khan <62823214+verycooljk007@users.noreply.github.com>
2025-03-26 00:57:03 -0400

    Add files via upload

---
+++ assignment2part2q1.py
@@ -0,0 +1,9 @@
+# Print numbers from 11 to 20 in a single line
+for i in range(11, 21):
+    print(i, end="")  # Print numbers in the same line with spaces
+
+print()  # Move to the next line
+
+# Print squares of numbers from 11 to 20 in a single line
+for i in range(11, 21):
+    print(i ** 2, end="")  # Print squares in the same line with spaces
